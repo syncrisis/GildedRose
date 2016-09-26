23b6b3f9af6a6e81b5be6448db57ae0febbcfd4b
Syncrisis <rolnickw@gmail.com>
2016-09-26 19:34:29 -0400

    Test Push

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -3,8 +3,8 @@ package com.gildedrose;
 class GildedRose {
 
     public static final String AGED_BRIE = "Aged Brie";
-    public static final String BACKSTAGE_PASSES_TO_A_TAFKAL80_ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
-    public static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
+    public static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
     public static final int MAX_QUALITY = 50;
     public static final int BACKSTAGE_DOUBLE_SELLIN = 11;
     public static final int BACKSTAGE_TRIPLE_SELLIN = 6;
@@ -16,8 +16,8 @@ class GildedRose {
 
     protected boolean itemQualityDecreasesByOne (Item item) {
         return !item.name.equals(AGED_BRIE)
-                && !item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80_ETC_CONCERT)
-                && !item.name.equals(SULFURAS_HAND_OF_RAGNAROS);
+                && !item.name.equals(BACKSTAGE_PASSES)
+                && !item.name.equals(SULFURAS);
     }
 
 
@@ -25,7 +25,8 @@ class GildedRose {
         for (int i = 0; i < items.length; i++) {
 
 
-            if (!items[i].name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+
+          if (!items[i].name.equals(SULFURAS)) {
                 items[i].sellIn = items[i].sellIn - 1;
             }
 
@@ -42,7 +43,7 @@ class GildedRose {
                 if (items[i].quality < MAX_QUALITY) {
                     items[i].quality = items[i].quality + 1;
 
-                    if (items[i].name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80_ETC_CONCERT)) {
+                    if (items[i].name.equals(BACKSTAGE_PASSES)) {
                         backstageUpdate(items[i]);
                     }
                 }
@@ -50,10 +51,13 @@ class GildedRose {
 
             // If we are past the sell by date
             if (items[i].sellIn < 0) {
-                if (itemQualityDecreasesByOne(items[i]) && ) {
-                    if (!items[i].name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80_ETC_CONCERT) &&
+                if (itemQualityDecreasesByOne(items[i])) {
+                    if (items[i].name.equals(BACKSTAGE_PASSES)) {
+                        items[i].quality = 0;
+                    }
+                    if (!items[i].name.equals(BACKSTAGE_PASSES) &&
                             items[i].quality > 0 &&
-                            !items[i].name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+                            !items[i].name.equals(SULFURAS)) {
                         items[i].quality = items[i].quality - 1;
                     }
                 } else {
@@ -64,6 +68,28 @@ class GildedRose {
             }
         }
     }
+
+
+    private void changeQuality (Item item, int change) {
+        int changedQuality = item.quality + change;
+        if (item.name == SULFURAS)
+        {
+            return;
+        }
+      if (changedQuality <= MAX_QUALITY && changedQuality >= 0) {
+
+        item.quality = changedQuality;
+      }
+      else if (changedQuality > MAX_QUALITY )
+      {
+        item.quality = MAX_QUALITY;
+      }
+      else if (changedQuality < 0)
+      {
+        item.quality = 0;
+      }
+    }
+
 
     private void backstageUpdate(Item item) {
         if (item.sellIn < BACKSTAGE_DOUBLE_SELLIN) {
