44dd74b7bec869003024842cdcc29848d9eb22c6
Syncrisis <rolnickw@gmail.com>
2016-09-28 19:27:30 -0400

    Mob Group Work Day 2

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -23,51 +23,95 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
+            int qualityChange = 0;
+            // If item changes it is not Sulfuras
+            if (items[i].name != SULFURAS) {
+                items[i].sellIn--;                 // Decrease sell in by 1
 
+                // if item degrades, it is not Aged Brie or Backstage Passes
+                if (items[i].name != AGED_BRIE && items[i].name != BACKSTAGE_PASSES) {
+                    // If sell in is >= 0
+                    if (items[i].sellIn >= 0) {
+                        qualityChange = -1;
+                    } else {
+                        qualityChange = -2;
+                    }
+                } else if (items[i].name == AGED_BRIE) {
+                    if (items[i].sellIn >= 0) {
+                        qualityChange = 1;
+                    } else {
+                        qualityChange = 2;
+                    }
 
+                } else if (items[i].name == BACKSTAGE_PASSES) {
+                    if (items[i].sellIn < 0) {
+                        qualityChange = -1 * items[i].quality;
+                    } else if (items[i].sellIn < BACKSTAGE_TRIPLE_SELLIN) {
+                        qualityChange = 3;
+                    } else if (items[i].sellIn < BACKSTAGE_DOUBLE_SELLIN) {
+                        qualityChange = 2;
+                    } else {
+                        qualityChange = 1;
+                    }
+                }
+                }
+                changeQuality(items[i], qualityChange);
 
-          if (!items[i].name.equals(SULFURAS)) {
-                items[i].sellIn = items[i].sellIn - 1;
             }
 
-            // If this is an ordinary decrease quality by one
-            if (itemQualityDecreasesByOne(items[i])) {
-                if (items[i].quality > 0 ) {
-                    items[i].quality = items[i].quality - 1;
-//                    if (items[i].sellIn < 0 && items[i].quality > 0) {
+        }
+
+
+
+//            if (!items[i].name.equals("Aged Brie")
+//                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+//                if (items[i].quality > 0) {
+//                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
 //                        items[i].quality = items[i].quality - 1;
 //                    }
-                }
-            // If this is not an ordinary item, increase by one or more
-            } else {
-                if (items[i].quality < MAX_QUALITY) {
-                    items[i].quality = items[i].quality + 1;
+//                }
+//            } else {
+//                if (items[i].quality < 50) {
+//                    items[i].quality = items[i].quality + 1;
+//
+//                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+//                        if (items[i].sellIn < 11) {
+//                            if (items[i].quality < 50) {
+//                                items[i].quality = items[i].quality + 1;
+//                            }
+//                        }
+//
+//                        if (items[i].sellIn < 6) {
+//                            if (items[i].quality < 50) {
+//                                items[i].quality = items[i].quality + 1;
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//
+//            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+//                items[i].sellIn = items[i].sellIn - 1;
+//            }
+//
+//            if (items[i].sellIn < 0) {
+//                if (!items[i].name.equals("Aged Brie")) {
+//                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+//                        if (items[i].quality > 0) {
+//                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+//                                items[i].quality = items[i].quality - 1;
+//                            }
+//                        }
+//                    } else {
+//                        items[i].quality = items[i].quality - items[i].quality;
+//                    }
+//                } else {
+//                    if (items[i].quality < 50) {
+//                        items[i].quality = items[i].quality + 1;
+//                    }
+//                }
+//            }
 
-                    if (items[i].name.equals(BACKSTAGE_PASSES)) {
-                        backstageUpdate(items[i]);
-                    }
-                }
-            }
-
-            // If we are past the sell by date
-            if (items[i].sellIn < 0) {
-                if (itemQualityDecreasesByOne(items[i])) {
-                    if (items[i].name.equals(BACKSTAGE_PASSES)) {
-                        items[i].quality = 0;
-                    }
-                    if (!items[i].name.equals(BACKSTAGE_PASSES) &&
-                            items[i].quality > 0 &&
-                            !items[i].name.equals(SULFURAS)) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                } else {
-                    if (items[i].quality < MAX_QUALITY) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
-        }
-    }
 
 
     private void changeQuality (Item item, int change) {
@@ -76,18 +120,17 @@ class GildedRose {
         {
             return;
         }
-      if (changedQuality <= MAX_QUALITY && changedQuality >= 0) {
-
-        item.quality = changedQuality;
-      }
-      else if (changedQuality > MAX_QUALITY )
-      {
-        item.quality = MAX_QUALITY;
-      }
-      else if (changedQuality < 0)
-      {
-        item.quality = 0;
-      }
+        if (changedQuality <= MAX_QUALITY && changedQuality >= 0) {
+            item.quality = changedQuality;
+        }
+        else if (changedQuality > MAX_QUALITY )
+        {
+            item.quality = MAX_QUALITY;
+        }
+        else if (changedQuality < 0)
+        {
+            item.quality = 0;
+        }
     }
 
 
